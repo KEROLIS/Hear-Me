c16262a0a756601fde1e423380949cec6f850947
KEROLIS <kerlisyoussif12@gmail.com>
2020-01-31 02:45:44 +0200

    Second Edit

--- app/src/main/java/com/example/deaf/Choose.java
+++ app/src/main/java/com/example/deaf/Choose.java
@@ -3,19 +3,22 @@ package com.example.deaf;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.MediaController;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.VideoView;
 
 public class Choose extends AppCompatActivity {
     Button button;
-    ImageView egy, kw, uae;
     TextView eg, k, u;
 
     @Override
@@ -25,46 +28,63 @@ public class Choose extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getSupportActionBar().hide();
         button = findViewById(R.id.next);
-        button.setEnabled(false);
-        egy = findViewById(R.id.egyPhoto);
-        kw = findViewById(R.id.kwaitPhoto);
-        uae = findViewById(R.id.uaePhoto);
-        eg = findViewById(R.id.egyTxt);
-        k = findViewById(R.id.kwaitTxt);
-        u = findViewById(R.id.uaeTxt);
+
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent mainIntent = new Intent(Choose.this, RealWork.class);
-                Choose.this.startActivity(mainIntent);
-                Choose.this.finish();
+                chooseVideoFromGallary();
+
+
             }
         });
 
 
     }
 
-    public void OnClick(View v) {
-        if (v.getId() == R.id.egyPhoto) {
-            eg.setTextColor(getResources().getColor(R.color.black));
-            k.setTextColor(getResources().getColor(R.color.white));
-            u.setTextColor(getResources().getColor(R.color.white));
-            button.setEnabled(true);
-            button.setAlpha(1);
+    public void chooseVideoFromGallary() {
+        Intent galleryIntent = new Intent(Intent.ACTION_PICK,
+                android.provider.MediaStore.Video.Media.EXTERNAL_CONTENT_URI);
 
-        } else if (v.getId() == R.id.kwaitPhoto) {
-            k.setTextColor(getResources().getColor(R.color.black));
-            eg.setTextColor(getResources().getColor(R.color.white));
-            u.setTextColor(getResources().getColor(R.color.white));
-            button.setEnabled(true);
-            button.setAlpha(1);
-        } else {
-            u.setTextColor(getResources().getColor(R.color.black));
-            eg.setTextColor(getResources().getColor(R.color.white));
-            k.setTextColor(getResources().getColor(R.color.white));
-            button.setEnabled(true);
-            button.setAlpha(1);
+        startActivityForResult(galleryIntent, 1);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        Log.d("result", "" + resultCode);
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == this.RESULT_CANCELED) {
+            Log.d("what", "cancle");
+            return;
+        }
+        Log.d("what", "gale");
+        if (data != null) {
+            Uri contentURI = data.getData();
+
+            String selectedVideoPath = getPath(contentURI);
+            Log.d("path", selectedVideoPath);
+//            saveVideoToInternalStorage(selectedVideoPath);
+            Toast.makeText(this, "جارى المعالجه", Toast.LENGTH_SHORT).show();
+            Intent mainIntent = new Intent(Choose.this, RealWork.class);
+            UriPass.contentURI=contentURI;
+            Choose.this.startActivity(mainIntent);
+            Choose.this.finish();
         }
     }
-}
+
+    public String getPath(Uri uri) {
+            String[] projection = {MediaStore.Video.Media.DATA};
+            Cursor cursor = getContentResolver().query(uri, projection, null, null, null);
+            if (cursor != null) {
+                // HERE YOU WILL GET A NULLPOINTER IF CURSOR IS NULL
+                // THIS CAN BE, IF YOU USED OI FILE MANAGER FOR PICKING THE MEDIA
+                int column_index = cursor
+                        .getColumnIndexOrThrow(MediaStore.Video.Media.DATA);
+                cursor.moveToFirst();
+                return cursor.getString(column_index);
+            } else
+                return null;
+        }
+    }
+
--- app/src/main/java/com/example/deaf/RealWork.java
+++ app/src/main/java/com/example/deaf/RealWork.java
@@ -1,21 +1,36 @@
 package com.example.deaf;
 
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
+import android.os.Handler;
 import android.provider.MediaStore;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.WindowManager;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ListView;
 import android.widget.MediaController;
+import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.Toolbar;
 import android.widget.VideoView;
 
 import java.io.File;
@@ -23,73 +38,64 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.ArrayList;
 import java.util.Calendar;
 
 import static android.media.MediaRecorder.VideoSource.CAMERA;
 
 public class RealWork extends AppCompatActivity {
     private static final String VIDEO_DIRECTORY = "/demonuts";
-    Button button;
     VideoView vid;
     private int GALLERY = 1, CAMERA = 2;
+    public static int[] drawer_icons = {R.drawable.splash};
+
+    TextView tv_selected_navigation,translationTV;
+    Button button;
+    ImageButton backBtn;
+    ArrayList<String> navigation_items;
+    private DrawerListAdapter drawerListAdapter;
+    private ListView lv_drawer;
+    int messeges[]={R.string.sign1,R.string.sign2,R.string.sign3,R.string.sign4,R.string.sign5,R.string.sign6};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_real_work2);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getSupportActionBar().hide();
-        button = findViewById(R.id.pro);
+        translationTV=findViewById(R.id.transTV);
+
         vid = findViewById(R.id.videoView);
 
-        button.setOnClickListener(new View.OnClickListener() {
+        backBtn= findViewById(R.id.backBtn);
+        backBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                showPictureDialog();
+
+                vid.setVisibility(View.INVISIBLE);
+                translationTV.setVisibility(View.INVISIBLE);
+                button.setVisibility(View.GONE);
+                chooseVideoFromGallary();
 
             }
         });
+
+        button=findViewById(R.id.againBtn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                chooseVideoFromGallary();
+            }
+        });
+        init();
+        SetDrawer();
     }
 
-    public void playVideo() {
-        MediaController m = new MediaController(this);
-        vid.setMediaController(m);
-
-        String path = "android.resource://com.example.deaf/" + R.raw.trail;
-
-        Uri u = Uri.parse(path);
-
-        vid.setVideoURI(u);
-
-        vid.start();
-
-    }
-
-    private void showPictureDialog() {
-        AlertDialog.Builder pictureDialog = new AlertDialog.Builder(this);
-        pictureDialog.setTitle("Select Action");
-        String[] pictureDialogItems = {
-                "Select video from gallery"};
-        pictureDialog.setItems(pictureDialogItems,
-                new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        chooseVideoFromGallary();
-                    }
-                });
-        pictureDialog.show();
-    }
 
     public void chooseVideoFromGallary() {
         Intent galleryIntent = new Intent(Intent.ACTION_PICK,
                 android.provider.MediaStore.Video.Media.EXTERNAL_CONTENT_URI);
 
         startActivityForResult(galleryIntent, GALLERY);
-    }
-
-    private void takeVideoFromCamera() {
-        Intent intent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
-        startActivityForResult(intent, CAMERA);
     }
 
     @Override
@@ -105,25 +111,33 @@ public class RealWork extends AppCompatActivity {
             Log.d("what", "gale");
             if (data != null) {
                 Uri contentURI = data.getData();
-
                 String selectedVideoPath = getPath(contentURI);
-                Log.d("path", selectedVideoPath);
-                saveVideoToInternalStorage(selectedVideoPath);
+                String fileName= selectedVideoPath.substring(selectedVideoPath.indexOf("sign"));
+                for (int i=1;i<7;i++){
+                if (fileName.equals("sign"+i+".mp4")){
+                    translationTV.setText(getString(messeges[i-1]));
+                    Log.d("FIle_name",fileName +"sign"+i+".mp4");
+                    break;}
+
+               }
                 Toast.makeText(this, "جارى المعالجه", Toast.LENGTH_SHORT).show();
                 vid.setVideoURI(contentURI);
-                vid.requestFocus();
-                vid.start();
+                button.setVisibility(View.GONE);
+                backBtn.setVisibility(View.VISIBLE);
+
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        /* Create an Intent that will start the Menu-Activity. */
+                        translationTV.setVisibility(View.VISIBLE);
+                        vid.setVisibility(View.VISIBLE);
+                        vid.requestFocus();
+                        vid.start();
+                    }
+                }, 1500);
 
             }
 
-        } else if (requestCode == CAMERA) {
-            Uri contentURI = data.getData();
-            String recordedVideoPath = getPath(contentURI);
-            Log.d("frrr", recordedVideoPath);
-            saveVideoToInternalStorage(recordedVideoPath);
-            vid.setVideoURI(contentURI);
-            vid.requestFocus();
-            vid.start();
         }
     }
 
@@ -177,6 +191,44 @@ public class RealWork extends AppCompatActivity {
             return cursor.getString(column_index);
         } else
             return null;
+    }
+
+    private void init() {
+        navigation_items = new ArrayList<>();
+
+        //adding menu items for naviations
+        navigation_items.add(getString(R.string.about));
+
+        lv_drawer = (ListView) findViewById(R.id.lv_drawer);
+
+    }
+
+    private void SetDrawer() {
+
+        drawerListAdapter = new DrawerListAdapter(RealWork.this, navigation_items, drawer_icons);
+        lv_drawer.setAdapter(drawerListAdapter);
+
+        lv_drawer.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                if(navigation_items.get(position).equalsIgnoreCase("Call")){
+
+                    tv_selected_navigation.setText("Selected Call");
+
+                }else if(navigation_items.get(position).equalsIgnoreCase("Favorite")){
+
+                    tv_selected_navigation.setText("Selected Favorite");
+
+                }else if(navigation_items.get(position).equalsIgnoreCase("Search")){
+
+                    tv_selected_navigation.setText("Selected Search");
+
+                }
+
+            }
+        });
+
     }
 
 }
